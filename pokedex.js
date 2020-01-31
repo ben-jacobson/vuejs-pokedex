f413ae9f62d118f149716ed9fdc10cf638cc94b9
ben-jacobson <ben_jacobson@live.com>
2020-01-31 21:54:45 +1100

    worked listview into swiper, experiencing some bugs with responsive elements and left/right button at the moment

--- pokedex.js
+++ pokedex.js
@@ -1,20 +1,21 @@
 Vue.component('poke-card', {
     props: ['pokemon'], 
-    template: `
-        <div class="pokecard vertical-margin-40px col-sm-4">
-            <div class="card">
-                <img class="card-img-top" :src="pokemon.sprites.front_default" alt="Bulbasaur">
-                <div class="card-body">
-                    <h5 class="card-title pokemon-name">#{{ pokemon.id }} {{ pokemon.name }}</h5>                                 
-                    <div class="poke-type grass-type"><b class="poke-type">Grass</b></div>
-                    <div class="poke-type poison-type"><b class="poke-type">Poison</b></div>                        
-                    <p class="card-text pokemon-description">{{ pokemon.description }}</p>
-                    <div class="text-center">
-                        <a href="#" class="btn btn-primary">More Info</a>
+    template: ` 
+            <div class="swiper-slide">
+                <div class="pokecard vertical-margin-40px col-sm-4">
+                    <div class="card">
+                        <img class="card-img-top" :src="pokemon.sprites.front_default" alt="Bulbasaur">
+                        <div class="card-body">
+                            <h5 class="card-title pokemon-name">#{{ pokemon.id }} {{ pokemon.name }}</h5>                                 
+                            <div class="poke-type grass-type"><b class="poke-type">Grass</b></div>
+                            <div class="poke-type poison-type"><b class="poke-type">Poison</b></div>                        
+                            <p class="card-text pokemon-description">{{ pokemon.description }}</p>                                    
+                            <div class="text-center"><a href="#" class="btn btn-primary">More Info</a></div>
+                        </div>
                     </div>
-                </div>
+                </div>                                    
             </div>
-        </div>`
+            `,
 });
 
 var pokemon_cards_vm = new Vue({ 
@@ -28,7 +29,7 @@ var pokemon_cards_vm = new Vue({
         
         // Pokemon data source
         max_pokemon: 151,  // first gen is the best gen!        
-        max_pokemon: 3,
+        //max_pokemon: 3,
         api_endpoint: 'https://pokeapi.co/api/v2/pokemon/', // append with ID        
         first_gen_pokemon: [],  // to store all first 151 gen pokemon
     },   
@@ -40,32 +41,21 @@ var pokemon_cards_vm = new Vue({
     computed: {
         // Filters our all undefined pokemon while the data is loading asynchronously. Our ajax request below needs to fill the array in a random order and a lot of null ids are found in the first_gen_pokemon array prior to all data loading. The solution is to filter them out with this computed value.
         ready_to_render_pokemon: function() {
-            var in_frame_boundary = this.in_frame_boundary;
+            //var in_frame_boundary = this.in_frame_boundary;
 
             return this.first_gen_pokemon.filter(function(data) {                 
-                return data.id != undefined && in_frame_boundary(data.id);  // only return the items that are ready to load and are in the current frame.
+                return data.id != undefined;// && in_frame_boundary(data.id);  // only return the items that are ready to load and are in the current frame.
             });
         },        
     },
 
     methods: {
-        next: function() {
-            if (this.x_pos + this.max_cards_in_frame <= this.max_pokemon) {
-                this.x_pos++;
-            }
-        }, 
-
-        prev: function() {
-            if (this.x_pos > 1) {
-                this.x_pos--;
-            }
-        },
-        
-        in_frame_boundary: function(id) {
+        /*in_frame_boundary: function(id) {
             return id >= this.x_pos && id < this.x_pos + this.max_cards_in_frame; 
-        },
+        },*/ 
 
         ajax_get_all_pokemon_listings: function() {
+            // no longer used, we instead use a function that runs the ajax requests of pokemon in the current frame, left and right frame.
             var vm = this;  // set this so that the promise has its own scoped copy of vm to work with after function has ended.                              
 
             // there is no API endpoint for retrieving a bulk of pokemon, you need to run individual requests. The rate limiter is generous enough at least
--- setup_swiper.js
+++ setup_swiper.js
@@ -1,8 +1,10 @@
 $(document).ready(function () {
     //initialize swiper when document ready
     var mySwiper = new Swiper ('.swiper-container', {
-        // Optional parameters
         direction: 'horizontal',
+        slidesPerView: 3,
+        spaceBetween: 30,
+
         loop: true,
         navigation: {
             nextEl: '.swiper-button-next',
