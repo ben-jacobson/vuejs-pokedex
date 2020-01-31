8f348010c1318df5a2f7b2e35bb8faacd190b917
ben-jacobson <ben_jacobson@live.com>
2020-01-31 20:07:49 +1100

    added swiper.js for card carousel feature

--- pokedex.js
+++ pokedex.js
@@ -18,14 +18,19 @@ Vue.component('poke-card', {
 });
 
 var pokemon_cards_vm = new Vue({ 
-    
+
     el: '#pokemon-cards',
 
     data: {
-        //max_pokemon: 10, 
+        // Carousel effect, show only 
+        max_cards_in_frame: 3, 
+        x_pos: 1,
+        
+        // Pokemon data source
         max_pokemon: 151,  // first gen is the best gen!        
+        max_pokemon: 3,
         api_endpoint: 'https://pokeapi.co/api/v2/pokemon/', // append with ID        
-        first_gen_pokemon: [], 
+        first_gen_pokemon: [],  // to store all first 151 gen pokemon
     },   
 
     created: function() {             
@@ -35,15 +40,33 @@ var pokemon_cards_vm = new Vue({
     computed: {
         // Filters our all undefined pokemon while the data is loading asynchronously. Our ajax request below needs to fill the array in a random order and a lot of null ids are found in the first_gen_pokemon array prior to all data loading. The solution is to filter them out with this computed value.
         ready_to_render_pokemon: function() {
-            return this.first_gen_pokemon.filter(function(data) { 
-                return data.id != undefined;
+            var in_frame_boundary = this.in_frame_boundary;
+
+            return this.first_gen_pokemon.filter(function(data) {                 
+                return data.id != undefined && in_frame_boundary(data.id);  // only return the items that are ready to load and are in the current frame.
             });
-        }
+        },        
     },
 
     methods: {
+        next: function() {
+            if (this.x_pos + this.max_cards_in_frame <= this.max_pokemon) {
+                this.x_pos++;
+            }
+        }, 
+
+        prev: function() {
+            if (this.x_pos > 1) {
+                this.x_pos--;
+            }
+        },
+        
+        in_frame_boundary: function(id) {
+            return id >= this.x_pos && id < this.x_pos + this.max_cards_in_frame; 
+        },
+
         ajax_get_all_pokemon_listings: function() {
-            var vm = this;  // fix for some weird scoping behaviour, saw this in the official vue doc too...                                    
+            var vm = this;  // set this so that the promise has its own scoped copy of vm to work with after function has ended.                              
 
             // there is no API endpoint for retrieving a bulk of pokemon, you need to run individual requests. The rate limiter is generous enough at least
             for (let i = 1; i <= this.max_pokemon; i++) {                  
@@ -56,6 +79,7 @@ var pokemon_cards_vm = new Vue({
 
                 // async to update the pokemon_data array, and ensure they are in the correct order. 
                 pokemon_data_promise.then(function(data) {
+                    data.name = data.name.charAt(0).toUpperCase() + data.name.substring(1); // a quick way to upper case the first letter of the pokemon name
                     Vue.set(vm.first_gen_pokemon, data.id - 1, data);   // a common gotcha, vue cannot be reactive to changes to array array[index] = value; 
                 });
             }            
---
+++ setup_swiper.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+});    
