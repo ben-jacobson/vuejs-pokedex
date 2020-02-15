432d8e2210d84a5aaab3a9a115fb93c4e8ec552b
ben-jacobson <ben_jacobson@live.com>
2020-02-16 10:31:07 +1100

    light refactor of code, making room for another ajax request

--- pokedex.js
+++ pokedex.js
@@ -1,3 +1,4 @@
+/* Some help functions */ 
 function capitalize_first_letter(text) {
     // capitalize the first letter of a word
     return text.charAt(0).toUpperCase() + text.substring(1);  
@@ -36,6 +37,7 @@ function json_property_to_string(json_data, identifier, string_to_extract) {
    return extracted_string;    
 }
 
+/* Our components */ 
 let pokemon_listview_card_component = {
     props: ['pokemon'],
     template: '#pokemon-listview-card-template',
@@ -43,7 +45,7 @@ let pokemon_listview_card_component = {
     methods: {
         more_info: function(e) {
             console.log(this.pokemon.id);
-            // emit an event
+            // emit an event with the id as payload.
         },
     }
 };
@@ -52,6 +54,8 @@ let pokemon_detailview_component = {
     props: ['pokemon'],
     template: '#pokemon-detailview-template',
 };
+
+/* Our Vue object */
 
 var pokemon_cards_vm = new Vue({ 
     el: '#pokedex',
@@ -66,41 +70,39 @@ var pokemon_cards_vm = new Vue({
         max_pokemon: 151,  // first gen is the best gen!        
         api_endpoint: 'https://pokeapi.co/api/v2/pokemon/', // append with ID        
         first_gen_pokemon: [],  // to store all first 151 gen pokemon
-        pokemon_currently_viewing: 15,
+        pokemon_currently_viewing: 10,
     },   
 
     created: function() {    
-        this.ajax_get_all_pokemon_listings();   
+        // there is no API endpoint for retrieving a list of pokemon, you need to run individual requests which seems silly. However, the rate limiter is generous enough and I haven't seen any issues as a result
+        for (let i = 1; i <= this.max_pokemon; i++) {    
+            Vue.set(this.first_gen_pokemon, i - 1, {id: i, loaded: false}); // pre-set an unloaded value
+            this.ajax_request_pokemon_data(i);
+        }       
     },
-
     methods: {
-        ajax_get_all_pokemon_listings: function() {
+        ajax_request_pokemon_data: function(id) {
             var vm = this;  // set this so that the promise has its own scoped copy of vm to work with after function has ended.                              
 
-            // there is no API endpoint for retrieving a bulk of pokemon, you need to run individual requests. The rate limiter is generous enough at least
-            for (let i = 1; i <= this.max_pokemon; i++) {    
-                Vue.set(vm.first_gen_pokemon, i - 1, {id: i, loaded: false}); // pre-set an unloaded value
+            // initiate the Ajax request. Returns the promise
+            var pokemon_data_promise = axios.get(this.api_endpoint + id + '/').then(function (response) {
+                return response.data;
+            }).catch(function (error) {
+                console.log(error);
+            }); 
 
-                // axios returns the promise
-                var pokemon_data_promise = axios.get(this.api_endpoint + i + '/').then(function (response) {
-                    return response.data;
-                }).catch(function (error) {
-                    console.log(error);
-                }); 
-
-                // async to update the pokemon_data array, and ensure they are in the correct order. 
-                pokemon_data_promise.then(function(data) {
-                    // make some changes to the data before loading into the array.
-                    data.name = capitalize_first_letter(data.name);
-                    data.weight = data.weight / 10; // weight is originally given in hectograms (100 grams)
-                    data.height = data.height * 10;      // height is originally given in decimeters
-                    data.types = json_property_to_string(data.types, "type", "name");
-                    data.abilities = json_property_to_string(data.abilities, "ability", "name");  
-                    data.description = "lorem ipsum dolor amet";                  
-                    data.loaded = true; // set a loaded flag
-                    Vue.set(vm.first_gen_pokemon, data.id - 1, data);   // a common gotcha, vue cannot be reactive to changes to array array[index] = value; 
-                });
-            }            
+            // async to update the pokemon_data array, and ensure they are in the correct order. 
+            pokemon_data_promise.then(function(data) {
+                // make some changes to the data before loading into the array.
+                data.name = capitalize_first_letter(data.name);
+                data.weight = data.weight / 10; // weight is originally given in hectograms (100 grams)
+                data.height = data.height * 10;      // height is originally given in decimeters
+                data.types = json_property_to_string(data.types, "type", "name");
+                data.abilities = json_property_to_string(data.abilities, "ability", "name");  
+                data.description = "Placeholder Description"; 
+                data.loaded = true; // set a loaded flag
+                Vue.set(vm.first_gen_pokemon, data.id - 1, data);   // a common gotcha, vue cannot be reactive to changes to array array[index] = value; 
+            });         
         },
     },    
 });
